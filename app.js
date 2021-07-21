1f5c9d46a79e04115a9c920f43ad5734f1178617
Oleg Paslavskiy <bulochka.dmg@mgail.com>
2021-07-21 13:53:15 +0300

    add click events and create function distance

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Click counter
+let clicks = 0;
+
 // Random number for map size
 let getRandomNumber = function(size) {
     return Math.floor(Math.random() * size);
@@ -11,3 +14,17 @@ let target = {
     x: getRandomNumber(width),
     y: getRandomNumber(height)
 };
+
+// Events
+$('#map').click(function(event) {
+    clicks++;
+});
+
+// The distance between the click and the treasure
+let getDistance = function(event, target) {
+    let diffX = event.offsetX - target.x;
+    let diffY = event.offsetY - target.y;
+    return Math.sqrt((diffX * diffX) + (diffY * diffY));
+};
+
+// How close to the treasure the player
