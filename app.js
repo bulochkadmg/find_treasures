4d849003c224df2160fcc907a177c1f25d031cd0
Oleg Paslavskiy <bulochka.dmg@mgail.com>
2021-07-21 12:35:28 +0300

    create random treasure coordinates

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+// Random number for map size
 let getRandomNumber = function(size) {
     return Math.floor(Math.random() * size);
 };
 
+// Treasure coordinates
+let width = 400;
+let height = 400;
+
+let target = {
+    x: getRandomNumber(width),
+    y: getRandomNumber(height)
+};
